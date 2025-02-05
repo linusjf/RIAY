b147cf16a08550f0918ae361c33b66c4fe2bd111
Linus Fernandes <linus.fernandes@gmail.com>
2025-02-05 11:37:38 +0530

    fix: use environment variable GIT_USER instead of hard-coding github userid

--- vidmd.sh
+++ vidmd.sh
@@ -33,7 +33,7 @@ playiconurl() {
   doy="$1"
   doy="$(printf "%03d" "${doy#0}")"
   month="$(mfromdoy "$doy")"
-  echo "https://raw.githubusercontent.com/linusjf/${root}/main/${month}/jpgs/Day${doy}.jpg"
+  echo "https://raw.githubusercontent.com/${GIT_USER}/${root}/main/${month}/jpgs/Day${doy}.jpg"
 }
 
 thumbnailurl() {
