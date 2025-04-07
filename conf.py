7cb41aa0bad2f566d614c10cd73bf1afeb23ad22
Linus Fernandes <linus.fernandes@gmail.com>
2025-04-08 05:10:11 +0530

    fix: trying latex for emojis
    fix: trying latex for emojis
    
    fix: trying latex for emojis
    
    fix: trying latex for emojis
    
    fix: trying latex for emojis
    
    fix: trying latex for emojis
    
    fix: trying latex for emojis
    
    fix: trying latex for emojis
    
    fix: trying latex for emojis
    
    fix: trying latex for emojis

--- conf.py
+++ conf.py
@@ -20,18 +20,36 @@ extensions = ["myst_parser"]
 templates_path = ['_templates']
 exclude_patterns = ["stitch.md","January/*.md","February/*.md", "March/*.md", "April/*.md", "May/*.md", "June/*.md", "July/*.md", "August/*.md", "September/*.md", "October/*.md"\
                     , "November/*.md", "December/*.md","README.md"]
+suppress_warnings = ['toc.not_included']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 
+html_exclude_files = ['book.rst']
 # -- Options for PDF output
-latex_engine = 'xelatex'
+latex_engine = 'lualatex'
 
 latex_elements = {
     'preamble': r'''
-\usepackage{fontspec}
-\setmainfont{Symbola}
+\usepackage{emoji}
 ''',
 }
+
+latex_documents = [
+    ('book', 'riay.tex', 'RIAY content', 'Linus Fernandes', 'manual'),
+]
+
+import sphinx
+
+# Default for HTML and EPUB
+master_doc = 'index'
+
+# If we're building LaTeX → override master_doc to 'book'
+if tags.has('latex'):
+    master_doc = 'book'
+
+# If we're building EPUB → exclude book.rst
+if tags.has('epub'):
+    epub_exclude_files = ['book.rst','README.md']
