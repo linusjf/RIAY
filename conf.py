520c70f3e6a02b79dd55e432208a932d16718b9e
Linus Fernandes <linus.fernandes@gmail.com>
2025-04-08 05:59:41 +0530

    fix: trying latex for emojis

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ extensions = ["myst_parser"]
 
 templates_path = ['_templates']
 exclude_patterns = ["stitch.md","January/*.md","February/*.md", "March/*.md", "April/*.md", "May/*.md", "June/*.md", "July/*.md", "August/*.md", "September/*.md", "October/*.md"\
-                    , "November/*.md", "December/*.md","README.md", "book.rst"]
+                    , "November/*.md", "December/*.md","README.md"]
 suppress_warnings = ['toc.not_included']
 
 # -- Options for HTML output -------------------------------------------------
@@ -28,6 +28,7 @@ suppress_warnings = ['toc.not_included']
 
 html_theme = 'alabaster'
 
+html_exclude_files = ['book.rst']
 # -- Options for PDF output
 latex_engine = 'lualatex'
 
