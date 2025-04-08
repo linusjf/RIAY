190a2eb3824ed53b1e9c7541fc9f8c0924d33a23
Linus Fernandes <linus.fernandes@gmail.com>
2025-04-08 05:44:06 +0530

    fix: trying latex for emojis

--- conf.py
+++ conf.py
@@ -21,6 +21,7 @@ extensions = ["myst_parser"]
 templates_path = ['_templates']
 exclude_patterns = ["stitch.md","January/*.md","February/*.md", "March/*.md", "April/*.md", "May/*.md", "June/*.md", "July/*.md", "August/*.md", "September/*.md", "October/*.md"\
                     , "November/*.md", "December/*.md","README.md"]
+suppress_warnings = ['toc.not_included']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
