1793eaab9976305f4100ccd5ae6b1593b1026661
Linus Fernandes <linus.fernandes@gmail.com>
2025-04-21 18:42:15 +0530

    feat: add function to make config variables readonly

---
+++ lockconfig.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+lock_config_vars() {
+  local config_file="$1"
+  local before_vars after_vars config_vars var
+
+  # Save current variable names before sourcing
+  before_vars="$(compgen -v)"
+  source "$config_file"
+  after_vars="$(compgen -v)"
+
+  # Get new variables introduced by the config
+  config_vars="$(comm -13 <(echo "$before_vars" | sort) <(echo "$after_vars" | sort))"
+
+  for var in $config_vars; do
+    # Check if it's an array
+    if declare -p "$var" 2> /dev/null | grep -q 'declare \-a'; then
+      # Lock all elements of the array
+      declare -r -a "$var"
+    else
+      readonly "$var"
+    fi
+  done
+}
