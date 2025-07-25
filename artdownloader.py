e93687e551ece65068f11582eaf17564f9547915
Linus Fernandes <linus.fernandes@gmail.com>
2025-07-25 18:13:56 +0530

    fix: fix log configuration so that logging is output to stderr

--- artdownloader.py
+++ artdownloader.py
@@ -30,13 +30,15 @@ from configenv import ConfigEnv
 from configconstants import ConfigConstants
 from PIL import Image
 
-# Configure logging to stderr
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    stream=sys.stderr
-)
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Configure logging to stderr
+if not logger.handlers:
+    handler = logging.StreamHandler(sys.stderr)
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    logger.addHandler(handler)
+    logger.propagate = False
 
 class ArtDownloader:
     """Download artwork images from various sources."""
